affa2755725511e34a9be66b429284f690af2c02
Yue Zhang <yuezh@ucdavis.edu>
2019-11-07 23:25:21 -0800

    updated

--- High-throughput Mutagenesis Reveals Unique Structural Features of Human ADAR1/CCHinputs/epiph_adr1.sh
+++
@@ -1,1 +0,0 @@
-/share/siegellab/software/Rosetta_rf/main/source/bin/rosetta_scripts.default.linuxgccrelease -database /share/siegellab/software/Rosetta_rf/main/database @flags -in:file:fasta /share/siegellab/tiffy/17_adr1/1_t01/adr1.fasta -parser:protocol hybridize.xml -out:file:silent adr1.out
